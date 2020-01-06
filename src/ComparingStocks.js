d2492c2d5f960b6278042e8fbaf1aec2ca48a17c
Rod Woodman <roderickwoodman@users.noreply.github.com>
2020-01-06 13:01:34 -0800

    Adds the top level component

---
+++ src/ComparingStocks.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+export class ComparingStocks extends React.Component {
+
+    render() {
+        return (
+            <div id="page-wrapper">
+                <div id="stocks-list">
+                    (FIXME: this is just placeholder code)
+                </div>
+            </div>
+        )
+    }
+
+}
