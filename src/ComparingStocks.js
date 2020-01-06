1edd739e2d533d46ef19ce12e864ef887a91bdf5
Rod Woodman <roderickwoodman@users.noreply.github.com>
2020-01-06 13:33:08 -0800

    Adds a simple table and a placeholder stocks list

--- src/ComparingStocks.js
+++ src/ComparingStocks.js
@@ -3,11 +3,38 @@ import React from 'react'
 
 export class ComparingStocks extends React.Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            allStocks: [ // FIXME: placeholder data for now
+                {'ticker': 'V', 'name': 'Visa Inc'},
+                {'ticker': 'MSFT', 'name': 'Microsoft Corporation'},
+                {'ticker': 'SBUX', 'name': 'Starbucks Corporation'},
+            ]
+        }
+    }
+
     render() {
+        const stocks = this.state.allStocks.map( stock => 
+            <tr key={ stock.ticker }>
+                <td>{ stock.ticker }</td>
+                <td>{ stock.name }</td>
+            </tr>
+        )
         return (
             <div id="page-wrapper">
                 <div id="stocks-list">
-                    (FIXME: this is just placeholder code)
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Ticker</th>
+                                <th>Company Name</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            { stocks }
+                        </tbody>
+                    </table>
                 </div>
             </div>
         )
