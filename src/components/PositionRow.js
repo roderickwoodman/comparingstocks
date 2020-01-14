36a0906eff7822bfc551973fcc31cf6cd3caaa75
Rod Woodman <roderickwoodman@users.noreply.github.com>
2020-01-13 22:21:26 -0800

    Replaces zeros with an inconspicuous placeholder character for readability

--- src/components/PositionRow.js
+++ src/components/PositionRow.js
@@ -5,6 +5,16 @@ import PropTypes from 'prop-types'
 export class PositionRow extends React.Component {
 
     render() {
+        function placeholderIfZero(value) {
+            let my_type = typeof(value)
+            if (my_type === 'number' && parseFloat(value) === 0) {
+                return '-'
+            } else if (my_type === 'string' && parseFloat(value.replace('$','')) === 0){
+                return '-'
+            } else {
+                return value
+            }
+        }
         const current_quote = this.props.current_quote
         let current_position = this.props.current_position
         if (current_position == null) {
@@ -20,10 +30,10 @@ export class PositionRow extends React.Component {
         return (
             <tr className={ row_classes }>
                 <td className="position-cell">{ current_quote.symbol }</td>
-                <td className="position-cell">{ current_position.current_shares }</td>
+                <td className="position-cell">{ placeholderIfZero(current_position.current_shares) }</td>
                 <td className="position-cell">${ current_quote.current_price }</td>
                 <td className="position-cell">{ current_quote.change_pct }%</td>
-                <td className="position-cell">${ (Math.round(100 * current_quote.current_price * current_position.current_shares) / 100).toFixed(2) }</td>
+                <td className="position-cell">{ placeholderIfZero('$' + (Math.round(100 * current_quote.current_price * current_position.current_shares) / 100).toFixed(2)) }</td>
                 <td className="position-cell">{ current_quote.volume }</td>
                 <td className="position-cell">{ performance.short_change_pct }%</td>
                 <td className="position-cell">{ performance.medium_change_pct }%</td>
