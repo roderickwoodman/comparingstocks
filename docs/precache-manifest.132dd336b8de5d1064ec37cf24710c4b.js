fa9479d6bc10f23713bbe3b5bef2b1d517a73322
Rod Woodman <roderickwoodman@users.noreply.github.com>
2020-01-14 21:46:36 -0800

    Deploys production code

---
+++ docs/precache-manifest.132dd336b8de5d1064ec37cf24710c4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1645561f4d9f448a50caba5f90f98f2",
+    "url": "/comparingstocks/index.html"
+  },
+  {
+    "revision": "a6e6231ee18732e6737a",
+    "url": "/comparingstocks/static/css/main.e27ae9f6.chunk.css"
+  },
+  {
+    "revision": "0652bd646e26c0fc2524",
+    "url": "/comparingstocks/static/js/2.df31a8c4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/comparingstocks/static/js/2.df31a8c4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a6e6231ee18732e6737a",
+    "url": "/comparingstocks/static/js/main.dd716c0a.chunk.js"
+  },
+  {
+    "revision": "03aaba1c4ec1eb8ccdff",
+    "url": "/comparingstocks/static/js/runtime-main.21a04d31.js"
+  }
+]);
