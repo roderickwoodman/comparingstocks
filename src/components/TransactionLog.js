3b58e4d98340976bbe0c84360706604b4cfd27b2
Rod Woodman <roderickwoodman@users.noreply.github.com>
2020-02-22 21:55:48 -0800

    Adds filter box for transaction log

--- src/components/TransactionLog.js
+++ src/components/TransactionLog.js
@@ -4,12 +4,35 @@ import PropTypes from 'prop-types'
 
 export class TransactionLog extends React.Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            filter_str: '',
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(event) {
+        const target = event.target
+        const new_value = target.value
+        const name = target.name
+        this.setState({ [name]: new_value })
+    }
+        
     render() {
         return (
             <section id="transaction-log">
-                {this.props.all_transactions.map( transaction => (
-                    <p key={transaction.modified} className="transaction" onClick={ (e)=>this.props.on_delete_transaction(transaction.modified)}>{transaction.summary}</p>
-                ))}
+                <section id="transaction-log-controls">
+                    <form>
+                        <label>Filter:</label>
+                        <input name="filter_str" value={this.state.filter_str} onChange={this.handleChange} size="15" />
+                    </form>
+                </section>
+                <section id="transactions">
+                    {this.props.all_transactions.filter( transaction => transaction.summary.includes(this.state.filter_str) ).map( transaction => (
+                        <p key={transaction.modified} className="transaction" onClick={ (e)=>this.props.on_delete_transaction(transaction.modified)}>{transaction.summary}</p>
+                    ))}
+                </section>
             </section>
         )
     }
