1dc9a077326e1546ab2e614c90ca2ec6132f5dfc
Rod Woodman <roderickwoodman@users.noreply.github.com>
2020-08-03 22:34:56 -0700

    Refactors Console component to use Hooks

--- src/components/Console.js
+++ src/components/Console.js
@@ -1,23 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 
-export class Console extends React.Component {
+export const Console = (props) => {
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            data_sort_dir: 'desc',
-            expanded_message_sets: []
-        }
-        this.getClasses = this.getClasses.bind(this)
-        this.getMessageSetClasses = this.getMessageSetClasses.bind(this)
-        this.formatTimestamp = this.formatTimestamp.bind(this)
-        this.onToggleExpandMessageSet = this.onToggleExpandMessageSet.bind(this)
-        this.onToggleSortOrder = this.onToggleSortOrder.bind(this)
-    }
+    const [dataSortDir, setDataSortDir] = useState('desc')
+    const [expandedMessageSets, setExpandedMessageSets] = useState([])
 
-    getClasses(message) {
+    const getClasses = (message) => {
         let classes = 'message'
         if (message.toUpperCase().startsWith('ERROR')) {
             classes += ' warning'
@@ -25,7 +15,7 @@ export class Console extends React.Component {
         return classes
     }
 
-    getMessageSetClasses(message_set_count) {
+    const getMessageSetClasses = (message_set_count) => {
         let classes = 'message_set'
         if (parseInt(message_set_count) > 1) {
             classes += ' multiline'
@@ -33,73 +23,64 @@ export class Console extends React.Component {
         return classes
     }
 
-    formatTimestamp(epoch) {
+    const formatTimestamp = (epoch) => {
+        console.log('epoch:',epoch)
         let tzoffset = (new Date()).getTimezoneOffset() * 60000; //offset in milliseconds
-        let localISOTime = (new Date(new Date(epoch) - tzoffset)).toISOString()
+        let localISOTime = (new Date(new Date(parseInt(epoch)) - tzoffset)).toISOString()
         let iso = localISOTime.match(/(\d{4}-\d{2}-\d{2})T(\d{2}:\d{2}:\d{2})/)
         return iso[1] + ' ' + iso[2]
     }
 
-    onToggleSortOrder() {
-        this.setState(prevState => {
-            let new_sort_dir = (prevState.data_sort_dir === 'asc') ? 'desc' : 'asc'
-            return { 
-                data_sort_dir: new_sort_dir 
-            }
-        })
+    const onToggleSortOrder = () => {
+        let new_sort_dir = (dataSortDir === 'asc') ? 'desc' : 'asc'
+        setDataSortDir(new_sort_dir)
     }
 
-    onToggleExpandMessageSet(identifier) {
-        this.setState(prevState => {
-            let new_expanded_message_sets = JSON.parse(JSON.stringify(prevState.expanded_message_sets))
-            if (!new_expanded_message_sets.includes(identifier)) {
-                new_expanded_message_sets = [identifier]
-            } else {
-                new_expanded_message_sets = []
-            }
-            return { expanded_message_sets: new_expanded_message_sets }
-        })
-    }
-
-    render() {
-
-        const PopulateMessageSet = ({message_set}) => {
-            let timestamp = this.formatTimestamp(message_set.modified_at)
-            let count = message_set.messages.length
-            return (
-                <div className={this.getMessageSetClasses(count)}>
-                    <p className="summary" onClick={ (e) => this.onToggleExpandMessageSet(timestamp)}>[{timestamp}] <span className={this.getClasses(message_set.summary)}>{message_set.summary}</span></p>
-                    { message_set.messages.length > 1 && this.state.expanded_message_sets.includes(timestamp) && message_set.messages.map ( (message, i) => (
-                        <p key={i} onClick={ (e) => this.onToggleExpandMessageSet(timestamp)}><span className={this.getClasses(message)}>{message}</span></p>
-                    ))}
-                </div>
-            )
+    const onToggleExpandMessageSet = (identifier) => {
+        let new_expanded_message_sets = JSON.parse(JSON.stringify(expandedMessageSets))
+        if (!new_expanded_message_sets.includes(identifier)) {
+            new_expanded_message_sets = [identifier]
+        } else {
+            new_expanded_message_sets = []
         }
+        setExpandedMessageSets(new_expanded_message_sets)
+    }
 
-        let message_sets = this.props.all_console_messages
-        let ordered_message_sets
-        let self = this
-        ordered_message_sets = message_sets.sort(function(a,b) {
-            if (a.modified_at < b.modified_at) {
-                return (self.state.data_sort_dir === 'asc') ? -1 : 1
-            } else if (a.modified_at > b.modified_at) {
-                return (self.state.data_sort_dir === 'asc') ? 1 : -1
-            } else {
-                return 0
-            }
-
-        })
+    const PopulateMessageSet = (to_populate) => {
+        let timestamp = formatTimestamp(to_populate.message_set.modified_at)
+        let count = to_populate.message_set.messages.length
         return (
-            <div id="console-messages-wrapper">
-                <button onClick={ (e)=>this.onToggleSortOrder() } className="strong">&#x21c5;</button> History:
-                <div id="console-messages">
-                { ordered_message_sets && ordered_message_sets.map( (message_set, i) => (
-                    <PopulateMessageSet key={i} message_set={message_set} />
+            <div className={getMessageSetClasses(count)}>
+                <p className="summary" onClick={ (e) => onToggleExpandMessageSet(timestamp)}>[{timestamp}] <span className={getClasses(to_populate.message_set.summary)}>{to_populate.message_set.summary}</span></p>
+                { to_populate.message_set.messages.length > 1 && expandedMessageSets.includes(timestamp) && to_populate.message_set.messages.map ( (message, i) => (
+                    <p key={i} onClick={ (e) => onToggleExpandMessageSet(timestamp)}><span className={getClasses(message)}>{message}</span></p>
                 ))}
-                </div>
             </div>
         )
     }
+
+    let message_sets = props.all_console_messages
+    let ordered_message_sets
+    ordered_message_sets = message_sets.sort(function(a,b) {
+        if (a.modified_at < b.modified_at) {
+            return (dataSortDir === 'asc') ? -1 : 1
+        } else if (a.modified_at > b.modified_at) {
+            return (dataSortDir === 'asc') ? 1 : -1
+        } else {
+            return 0
+        }
+    })
+
+    return (
+        <div id="console-messages-wrapper">
+            <button onClick={ (e)=>onToggleSortOrder() } className="strong">&#x21c5;</button> History:
+            <div id="console-messages">
+            { ordered_message_sets && ordered_message_sets.map( (message_set, i) => (
+                <PopulateMessageSet key={i} message_set={message_set} />
+            ))}
+            </div>
+        </div>
+    )
 }
 
 Console.propTypes = {
