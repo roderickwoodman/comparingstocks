a63e63e6452501edc2489a25237b6b37607ef098
Rod Woodman <roderickwoodman@users.noreply.github.com>
2020-08-07 23:21:02 -0700

    Refactors TagDelete component to use Hooks

--- src/components/TagDelete.js
+++ src/components/TagDelete.js
@@ -1,20 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 
-export class TagDelete extends React.Component {
+export const TagDelete = (props) => {
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            delete_tag_list: ['untagged'],
-        }
-        this.handleTagChange = this.handleTagChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-        this.isDisabled = this.isDisabled.bind(this)
-    }
+    const [deleteTagList, setDeleteTagList] = useState(['untagged'])
 
-    handleTagChange(event) {
+    const handleTagChange = (event) => {
         let {selectedOptions } = event.target
 
         let multiple_tags = Array.from(selectedOptions, (item) => item.value)
@@ -22,46 +14,44 @@ export class TagDelete extends React.Component {
             multiple_tags = ['untagged']
         }
 
-        this.setState({ delete_tag_list: multiple_tags })
+        setDeleteTagList(multiple_tags)
     }
 
-    handleSubmit(event) {
+    const handleSubmit = (event) => {
         event.preventDefault()
 
-        let user_tag_list = this.state.delete_tag_list
-        this.props.on_delete_tags(user_tag_list)
+        let user_tag_list = deleteTagList
+        props.on_delete_tags(user_tag_list)
 
-        this.setState({ delete_tag_list: ['untagged'] })
+        setDeleteTagList(['untagged'])
     }
 
-    isDisabled() {
-        if (this.state.delete_tag_list.length === 1 && this.state.delete_tag_list.includes('untagged')) {
+    const isDisabled = () => {
+        if (deleteTagList.length === 1 && deleteTagList.includes('untagged')) {
             return true
         } else {
             return false
         }
     }
 
-    render() {
-        return (
-            <section id="delete-tag">
-                <form onSubmit={this.handleSubmit}>
-                    <div id="operation">
-                        Delete Tag:
-                        <select value={this.state.delete_tag_list} multiple={true} onChange={this.handleTagChange}>
-                            <option key="untagged" value="untagged">(none)</option>
-                            {Object.keys(this.props.all_tags).sort().filter(tag_name => tag_name !== 'untagged').map(tag_name => (
-                            <option key={tag_name} value={tag_name}>{tag_name}</option>
-                            ))}
-                        </select>
-                    </div>
-                    <section className="buttonrow">
-                        <input className="btn btn-sm btn-primary" type="submit" value="Delete Tag(s)" disabled={this.isDisabled()} />
-                    </section>
-                </form>
-            </section>
-        )
-    }
+    return (
+        <section id="delete-tag">
+            <form onSubmit={handleSubmit}>
+                <div id="operation">
+                    Delete Tag:
+                    <select value={deleteTagList} multiple={true} onChange={handleTagChange}>
+                        <option key="untagged" value="untagged">(none)</option>
+                        {Object.keys(props.all_tags).sort().filter(tag_name => tag_name !== 'untagged').map(tag_name => (
+                        <option key={tag_name} value={tag_name}>{tag_name}</option>
+                        ))}
+                    </select>
+                </div>
+                <section className="buttonrow">
+                    <input className="btn btn-sm btn-primary" type="submit" value="Delete Tag(s)" disabled={isDisabled()} />
+                </section>
+            </form>
+        </section>
+    )
 }
 
 TagDelete.propTypes = {
