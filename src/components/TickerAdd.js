a8d456087b18d0b379cab82b8fa39e363620ac59
Rod Woodman <roderickwoodman@users.noreply.github.com>
2020-08-07 23:20:13 -0700

    Refactors TickerAdd component to use Hooks

--- src/components/TickerAdd.js
+++ src/components/TickerAdd.js
@@ -1,56 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 
-export class TickerAdd extends React.Component {
+export const TickerAdd = (props) => {
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            user_tickers_string: '',
-            add_to_tag: 'untagged',
-        }
-        this.handleTickersChange = this.handleTickersChange.bind(this)
-        this.handleTagChange = this.handleTagChange.bind(this)
-        this.handleReset = this.handleReset.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-        this.validateTickers = this.validateTickers.bind(this)
+    const [userTickersString, setUserTickersString] = useState('')
+    const [addToTag, setAddToTag] = useState('untagged')
+
+    const handleTickersChange = (event) => {
+        setUserTickersString(event.target.value)
     }
 
-    handleTickersChange(event) {
-        this.setState({ user_tickers_string: event.target.value })
+    const handleTagChange = (event) => {
+        setAddToTag(event.target.value)
     }
 
-    handleTagChange(event) {
-        this.setState({ add_to_tag: event.target.value })
+    const handleReset = (event) => {
+        setUserTickersString('')
     }
 
-    handleReset(event) {
-        this.setState({ user_tickers_string: "" })
-    }
-
-    handleSubmit(event) {
+    const handleSubmit = (event) => {
         event.preventDefault()
-        let user_tag = this.state.add_to_tag
-        let user_tickers = String(this.state.user_tickers_string)
+        let user_tag = addToTag
+        let user_tickers = String(userTickersString)
             .split(" ")
             .map(str => str.trim())
             .map(str => str.toUpperCase())
             .map(str => str.replace(/[^A-Z]/g, ""))
-        this.validateTickers(user_tag, Array.from(new Set(user_tickers)))
+        validateTickers(user_tag, Array.from(new Set(user_tickers)))
     }
 
-    validateTickers(tag, tickers) {
+    const validateTickers = (tag, tickers) => {
         let tickers_to_add = []
         let new_messages = []
-        let self = this
         tickers.forEach(function(ticker) {
             // ticker does not exist
-            if (!self.props.all_stocks.includes(ticker)) {
+            if (!props.all_stocks.includes(ticker)) {
                 new_messages.push('ERROR: Ticker ' + ticker + ' does not exist.')
 
             // ticker is already in the target tag
-            } else if (self.props.all_tags[tag].includes(ticker)) {
+            } else if (props.all_tags[tag].includes(ticker)) {
                 if (tag === 'untagged') {
                     new_messages.push('ERROR: Ticker ' + ticker + ' has already been added.')
                 } else {
@@ -60,9 +49,9 @@ export class TickerAdd extends React.Component {
             // ticker is being added to a tag that it is not already in
             } else {
                 let tagged_tickers = []
-                Object.keys(self.props.all_tags).forEach(function(tag) {
+                Object.keys(props.all_tags).forEach(function(tag) {
                     if (tag !== 'untagged') {
-                        tagged_tickers = tagged_tickers.concat(self.props.all_tags[tag])
+                        tagged_tickers = tagged_tickers.concat(props.all_tags[tag])
                     }
                 })
                 if (tag === 'untagged' && tagged_tickers.includes(ticker)) {
@@ -87,40 +76,38 @@ export class TickerAdd extends React.Component {
         } else {
             summary = 'ERROR: ' + num_errors + ' of ' + tickers.length + ' tickers could not be added' + tag_status_str + '.'
         }
-        let new_console_message_set = this.props.create_console_message_set(summary)
+        let new_console_message_set = props.create_console_message_set(summary)
         if (new_messages.length > 1) {
             new_console_message_set.messages = [...new_messages]
         }
         if (num_errors > 0) {
             new_console_message_set.has_errors = true
         }
-        this.props.on_new_tickers(tag, tickers_to_add)
-        this.props.on_new_console_messages(new_console_message_set)
-        this.handleReset()
+        props.on_new_tickers(tag, tickers_to_add)
+        props.on_new_console_messages(new_console_message_set)
+        handleReset()
     }
 
-    render() {
-        return (
-            <section id="add-ticker">
-                <form onSubmit={this.handleSubmit} onReset={this.handleReset}>
-                    <label>New Ticker(s):</label>
-                    <input value={this.state.user_tickers_string} onChange={this.handleTickersChange} placeholder="Dow30 tickers only" required />
-                    <label>
-                        Add to Tag:
-                        <select value={this.state.add_to_tag} onChange={this.handleTagChange}>
-                            <option key="untagged" value="untagged">(no tag)</option>
-                            {Object.keys(this.props.all_tags).sort().filter(tag_name => tag_name !== 'untagged').map(tag_name => (
-                            <option key={tag_name} value={tag_name}>{tag_name}</option>
-                            ))}
-                        </select>
-                    </label>
-                    <section className="buttonrow">
-                        <input className="btn btn-sm btn-primary" type="submit" value="Add Ticker(s)" disabled={this.state.user_tickers_string===''}/>
-                    </section>
-                </form>
-            </section>
-        )
-    }
+    return (
+        <section id="add-ticker">
+            <form onSubmit={handleSubmit} onReset={handleReset}>
+                <label>New Ticker(s):</label>
+                <input value={userTickersString} onChange={handleTickersChange} placeholder="Dow30 tickers only" required />
+                <label>
+                    Add to Tag:
+                    <select value={addToTag} onChange={handleTagChange}>
+                        <option key="untagged" value="untagged">(no tag)</option>
+                        {Object.keys(props.all_tags).sort().filter(tag_name => tag_name !== 'untagged').map(tag_name => (
+                        <option key={tag_name} value={tag_name}>{tag_name}</option>
+                        ))}
+                    </select>
+                </label>
+                <section className="buttonrow">
+                    <input className="btn btn-sm btn-primary" type="submit" value="Add Ticker(s)" disabled={userTickersString===''}/>
+                </section>
+            </form>
+        </section>
+    )
 }
 
 TickerAdd.propTypes = {
