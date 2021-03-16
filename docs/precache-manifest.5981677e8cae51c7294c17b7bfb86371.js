1bb74ebdb96dd25877d87e07ac7d917aad5de0fd
Rod Woodman <roderickwoodman@users.noreply.github.com>
2021-03-15 23:33:33 -0700

    Deploys production code

---
+++ docs/precache-manifest.5981677e8cae51c7294c17b7bfb86371.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73b897b98a8795dc4d30e6b7a6f4b238",
+    "url": "/comparingstocks/index.html"
+  },
+  {
+    "revision": "89f83390bbe68b3ec5bb",
+    "url": "/comparingstocks/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "6b4478d78778841f00ad",
+    "url": "/comparingstocks/static/css/main.6a9eb8c5.chunk.css"
+  },
+  {
+    "revision": "89f83390bbe68b3ec5bb",
+    "url": "/comparingstocks/static/js/2.a6743f72.chunk.js"
+  },
+  {
+    "revision": "836687f75dbf024cab882ad1c8c0e52b",
+    "url": "/comparingstocks/static/js/2.a6743f72.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6b4478d78778841f00ad",
+    "url": "/comparingstocks/static/js/main.82743c49.chunk.js"
+  },
+  {
+    "revision": "e681e1ce5432dc11ceb9",
+    "url": "/comparingstocks/static/js/runtime-main.81ce229d.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/comparingstocks/precache-manifest.31561330558139b724878ca9548d39aa.js"
+  "/comparingstocks/precache-manifest.5981677e8cae51c7294c17b7bfb86371.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/comparingstocks/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
